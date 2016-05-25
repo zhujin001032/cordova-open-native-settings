15b1ee2038f6465083a911a0874530d42f650402
Guy Rombaut <guy.rombaut@avg.com>
2016-05-25 13:22:10 +0200

    ios 9 test

--- src/ios/NativeSettings.m
+++ src/ios/NativeSettings.m
@@ -9,7 +9,7 @@
 	if (![[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=Bluetooth"]]) {
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
 	} else {
-		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"Opened"];
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Opened"];
 	}
 	
 	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
