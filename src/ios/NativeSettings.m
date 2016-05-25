75bdf0adc019f0a2d19ff505a8723522dfd45a05
Guy Rombaut <guy.rombaut@avg.com>
2016-05-25 13:40:42 +0200

    a

--- src/ios/NativeSettings.m
+++ src/ios/NativeSettings.m
@@ -6,10 +6,10 @@
 - (void)open:(CDVInvokedUrlCommand*)command
 {
 	CDVPluginResult* pluginResult = nil;
-	if (![[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=Bluetooth"]]) {
-		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-	} else {
+	if ([[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=General&path=Keyboard"]]) {
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Opened"];
+	} else {
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
 	}
 	
 	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
