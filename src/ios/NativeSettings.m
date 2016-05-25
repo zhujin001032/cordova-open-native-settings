cdb7ddb364b3f5b88391ba2675c2541e004dc6e2
Guy Rombaut <guy.rombaut@avg.com>
2016-05-25 16:44:30 +0200

    add self decleration for do open

--- src/ios/NativeSettings.m
+++ src/ios/NativeSettings.m
@@ -15,127 +15,127 @@
 {
 	CDVPluginResult* pluginResult = nil;
 	NSString* key = [command.arguments objectAtIndex:0];
-	BOOL result = nil;
+	BOOL result = NO;
 	
 	if ([key isEqualToString:@"settings"]) {
-		result = do_open(@"prefs:");
+		result = self do_open(@"prefs:");
 	}
 	else if ([key isEqualToString:@"about"]) {
-		result = do_open(@"prefs:root=General&path=About");
+		result = self do_open(@"prefs:root=General&path=About");
 	}
 	else if ([key isEqualToString:@"accessibility"]) {
-		result = do_open(@"prefs:root=General&path=ACCESSIBILITY");
+		result = self do_open(@"prefs:root=General&path=ACCESSIBILITY");
 	}
 	else if ([key isEqualToString:@"account"]) {
-		result = do_open(@"prefs:root=ACCOUNT_SETTINGS");
+		result = self do_open(@"prefs:root=ACCOUNT_SETTINGS");
 	}
 	else if ([key isEqualToString:@"airplane_mode"]) {
-		result = do_open(@"prefs:root=AIRPLANE_MODE");
+		result = self do_open(@"prefs:root=AIRPLANE_MODE");
 	}
 	else if ([key isEqualToString:@"autolock"]) {
-		result = do_open(@"prefs:root=General&path=AUTOLOCK");
+		result = self do_open(@"prefs:root=General&path=AUTOLOCK");
 	}
 	else if ([key isEqualToString:@"display"]) {
-		result = do_open(@"prefs:root=Brightness");
+		result = self do_open(@"prefs:root=Brightness");
 	}
 	else if ([key isEqualToString:@"bluetooth"]) {
-		result = do_open(@"prefs:root=General&path=Bluetooth");
+		result = self do_open(@"prefs:root=General&path=Bluetooth");
 	}
 	else if ([key isEqualToString:@"castle"]) {
-		result = do_open(@"prefs:root=CASTLE");
+		result = self do_open(@"prefs:root=CASTLE");
 	}
 	else if ([key isEqualToString:@"cellular_usage"]) {
-		result = do_open(@"prefs:root=General&path=USAGE/CELLULAR_USAGE");
+		result = self do_open(@"prefs:root=General&path=USAGE/CELLULAR_USAGE");
 	}
 	else if ([key isEqualToString:@"configuration_list"]) {
-		result = do_open(@"prefs:root=General&path=ManagedConfigurationList");
+		result = self do_open(@"prefs:root=General&path=ManagedConfigurationList");
 	}
 	else if ([key isEqualToString:@"date"]) {
-		result = do_open(@"prefs:root=General&path=DATE_AND_TIME");
+		result = self do_open(@"prefs:root=General&path=DATE_AND_TIME");
 	}
 	else if ([key isEqualToString:@"facetime"]) {
-		result = do_open(@"prefs:root=FACETIME");
+		result = self do_open(@"prefs:root=FACETIME");
 	}
 	else if ([key isEqualToString:@"settings"]) {
-		result = do_open(@"prefs:root=General");
+		result = self do_open(@"prefs:root=General");
 	}
 	else if ([key isEqualToString:@"tethering"]) {
-		result = do_open(@"prefs:root=INTERNET_TETHERING");
+		result = self do_open(@"prefs:root=INTERNET_TETHERING");
 	}
 	else if ([key isEqualToString:@"music"]) {
-		result = do_open(@"prefs:root=MUSIC");
+		result = self do_open(@"prefs:root=MUSIC");
 	}
 	else if ([key isEqualToString:@"music_equalizer"]) {
-		result = do_open(@"prefs:root=MUSIC&path=EQ");
+		result = self do_open(@"prefs:root=MUSIC&path=EQ");
 	}
 	else if ([key isEqualToString:@"music_volume"]) {
-		result = do_open(@"prefs:root=MUSIC&path=VolumeLimit");
+		result = self do_open(@"prefs:root=MUSIC&path=VolumeLimit");
 	}
 	else if ([key isEqualToString:@"keyboard"]) {
-		result = do_open(@"prefs:root=General&path=Keyboard");
+		result = self do_open(@"prefs:root=General&path=Keyboard");
 	}
 	else if ([key isEqualToString:@"locale"]) {
-		result = do_open(@"prefs:root=General&path=INTERNATIONAL");
+		result = self do_open(@"prefs:root=General&path=INTERNATIONAL");
 	}
 	else if ([key isEqualToString:@"location"]) {
-		result = do_open(@"prefs:root=LOCATION_SERVICES");
+		result = self do_open(@"prefs:root=LOCATION_SERVICES");
 	}
 	else if ([key isEqualToString:@"network"]) {
-		result = do_open(@"prefs:root=General&path=Network");
+		result = self do_open(@"prefs:root=General&path=Network");
 	}
 	else if ([key isEqualToString:@"nike_ipod"]) {
-		result = do_open(@"prefs:root=NIKE_PLUS_IPOD");
+		result = self do_open(@"prefs:root=NIKE_PLUS_IPOD");
 	}
 	else if ([key isEqualToString:@"notes"]) {
-		result = do_open(@"prefs:root=NOTES");
+		result = self do_open(@"prefs:root=NOTES");
 	}
 	else if ([key isEqualToString:@"notification_id"]) {
-		result = do_open(@"prefs:root=NOTIFICATIONS_ID");
+		result = self do_open(@"prefs:root=NOTIFICATIONS_ID");
 	}
 	else if ([key isEqualToString:@"passbook"]) {
-		result = do_open(@"prefs:root=PASSBOOK");
+		result = self do_open(@"prefs:root=PASSBOOK");
 	}
 	else if ([key isEqualToString:@"phone"]) {
-		result = do_open(@"prefs:root=Phone");
+		result = self do_open(@"prefs:root=Phone");
 	}
 	else if ([key isEqualToString:@"photos"]) {
-		result = do_open(@"prefs:root=Photos");
+		result = self do_open(@"prefs:root=Photos");
 	}
 	else if ([key isEqualToString:@"reset"]) {
-		result = do_open(@"prefs:root=General&path=Reset");
+		result = self do_open(@"prefs:root=General&path=Reset");
 	}
 	else if ([key isEqualToString:@"ringtone"]) {
-		result = do_open(@"prefs:root=Sounds&path=Ringtone");
+		result = self do_open(@"prefs:root=Sounds&path=Ringtone");
 	}
 	else if ([key isEqualToString:@"browser"]) {
-		result = do_open(@"prefs:root=Safari");
+		result = self do_open(@"prefs:root=Safari");
 	}
 	else if ([key isEqualToString:@"search"]) {
-		result = do_open(@"prefs:root=General&path=Assistant");
+		result = self do_open(@"prefs:root=General&path=Assistant");
 	}
 	else if ([key isEqualToString:@"sound"]) {
-		result = do_open(@"prefs:root=Sounds");
+		result = self do_open(@"prefs:root=Sounds");
 	}
 	else if ([key isEqualToString:@"software_update"]) {
-		result = do_open(@"prefs:root=General&path=SOFTWARE_UPDATE_LINK");
+		result = self do_open(@"prefs:root=General&path=SOFTWARE_UPDATE_LINK");
 	}
 	else if ([key isEqualToString:@"storage"]) {
-		result = do_open(@"prefs:root=CASTLE&path=STORAGE_AND_BACKUP");
+		result = self do_open(@"prefs:root=CASTLE&path=STORAGE_AND_BACKUP");
 	}
 	else if ([key isEqualToString:@"store"]) {
-		result = do_open(@"prefs:root=STORE");
+		result = self do_open(@"prefs:root=STORE");
 	}
 	else if ([key isEqualToString:@"usage"]) {
-		result = do_open(@"prefs:root=General&path=USAGE");
+		result = self do_open(@"prefs:root=General&path=USAGE");
 	}
 	else if ([key isEqualToString:@"video"]) {
-		result = do_open(@"prefs:root=VIDEO");
+		result = self do_open(@"prefs:root=VIDEO");
 	}
 	else if ([key isEqualToString:@"vpn"]) {
-		result = do_open(@"prefs:root=General&path=Network/VPN");
+		result = self do_open(@"prefs:root=General&path=Network/VPN");
 	}
 	else if ([key isEqualToString:@"wallpaper"]) {
-		result = do_open(@"prefs:root=Wallpaper");
+		result = self do_open(@"prefs:root=Wallpaper");
 	} 
 	else {
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid Action"];
