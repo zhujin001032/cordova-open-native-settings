f137af156123b3bc54aa92c5d1a14c221975640b
Guy Rombaut <guy.rombaut@avg.com>
2016-08-08 15:26:39 +0200

    open in new window parameter for android

--- www/settings.js
+++ www/settings.js
@@ -2,7 +2,7 @@ var NativeSettings = function() {
 };
 
 NativeSettings.open = function(setting, onsucess, onfail) {
-	settings = (typeof setting == 'string' || setting instanceof String) ? [setting] : setting;
+	var settings = (typeof setting === 'string' || setting instanceof String) ? [setting] : setting;
 	cordova.exec(onsucess, onfail, "NativeSettings", "open", settings);
 };
 
